54bf497b9fee2d302211ee6a635fe9e435164484
NyxCormo <laine.clement@outlook.fr>
2026-03-16 23:14:00 +0100

    Autoclick is now NxM generalized

--- src/runner/apply_solution.py
+++ src/runner/apply_solution.py
@@ -1,18 +1,19 @@
 import pyautogui
 import time
 
-def click_cell(cell, hold_time=0.003):
+def click_cell(cell):
     cx = cell["x"] + cell["w"] // 2
     cy = cell["y"] + cell["h"] // 2
     pyautogui.mouseDown(cx, cy)
-    time.sleep(hold_time)
     pyautogui.mouseUp(cx, cy)
 
 def apply_solution_on_screen(picross_data, solution):
     time.sleep(0.2)
     cells = picross_data["cells"]
+    height, width = solution.shape
+
     for idx, cell in enumerate(cells):
-        r, c = divmod(idx, 5)
+        r = idx // width
+        c = idx % width
         if solution[r, c] == 1:
             click_cell(cell)
-            time.sleep(0.01)
